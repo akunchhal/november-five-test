ae808a3a862ac4955ff721186f02b1e6999e6fff
Ankita Kunchhal <ankitakunchhal@gmail.com>
2023-09-03 18:30:56 +0100

    add defaultCommandTimeout to 10000 in cypress config
    Signed-off-by: Ankita Kunchhal <ankitakunchhal@gmail.com>

--- cypress.config.js
+++ cypress.config.js
@@ -3,5 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "https://main--nove-movie-reviews.netlify.app",
+    // Added this because site is taking time to load after login
+    defaultCommandTimeout: 10000,
   },
 });
